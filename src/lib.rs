d48076ec69a86e82abeca5f44c0b73ada1fda848
Paul Colomiets <paul@colomiets.name>
2022-04-06 15:02:31 +0300

    More comprehensive `serde_str::opt` implementation

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,13 @@
 //! #[derive(Serialize, Deserialize)]
 //! # #[derive(PartialEq, Debug)]
 //! struct WithIp {
-//! 	#[serde(with = "serde_str")]
-//! 	ip: IpAddr,
+//!     #[serde(with = "serde_str")]
+//!     ip: IpAddr,
 //! }
 //!
 //! use serde_json::{
-//! 	from_str,
-//! 	to_string,
+//!     from_str,
+//!     to_string,
 //! };
 //! # fn main() -> serde_json::Result<()> {
 //! let with_ip: WithIp = from_str(r#"{"ip": "127.0.0.1"}"#)?;
@@ -37,42 +37,42 @@
 //! # Ok(())
 //! # }
 //! ```
-#![forbid(missing_docs, missing_debug_implementations, unsafe_code, dead_code)]
-#![deny(unused)]
+#![forbid(unsafe_code)]
+#![warn(missing_docs, missing_debug_implementations)]
 use serde::{
-	de::{
-		Deserialize,
-		Error as DeserializeError,
-	},
-	ser::Serialize,
-	Deserializer,
-	Serializer,
+    de::{
+        Deserialize,
+        Error as DeserializeError,
+    },
+    ser::Serialize,
+    Deserializer,
+    Serializer,
 };
 use std::{
-	fmt,
-	str::FromStr,
+    fmt,
+    str::FromStr,
 };
 
 /// Deserialize function, see [crate docs examples](https://docs.rs/serde_str) to see how to use it
 pub fn deserialize<'de, D, T: FromStr>(deserializer: D) -> Result<T, D::Error>
 where
-	D: Deserializer<'de>,
-	<T as FromStr>::Err: fmt::Display,
+    D: Deserializer<'de>,
+    <T as FromStr>::Err: fmt::Display,
 {
-	let s = String::deserialize(deserializer)?;
-	T::from_str(&s).map_err(DeserializeError::custom)
+    let s = String::deserialize(deserializer)?;
+    T::from_str(&s).map_err(DeserializeError::custom)
 }
 
 /// Serialize function, see [crate docs examples](https://docs.rs/serde_str) to see how to use it
 pub fn serialize<S, T: ToString>(
-	value: &T,
-	serializer: S,
+    value: &T,
+    serializer: S,
 ) -> Result<S::Ok, S::Error>
 where
-	S: Serializer,
+    S: Serializer,
 {
-	let s = value.to_string();
-	String::serialize(&s, serializer)
+    let s = value.to_string();
+    String::serialize(&s, serializer)
 }
 
 pub mod emp;
--- src/opt.rs
+++ src/opt.rs
@@ -10,13 +10,13 @@
 //! #[derive(Serialize, Deserialize)]
 //! # #[derive(PartialEq, Debug)]
 //! struct WithIp {
-//! 	#[serde(with = "serde_str::opt")]
-//! 	ip: Option<IpAddr>,
+//!     #[serde(with = "serde_str::opt")]
+//!     ip: Option<IpAddr>,
 //! }
 //!
 //! use serde_json::{
-//! 	from_str,
-//! 	to_string,
+//!     from_str,
+//!     to_string,
 //! };
 //! # fn main() -> serde_json::Result<()> {
 //! let with_ip: WithIp = from_str(r#"{"ip": "127.0.0.1"}"#)?;
@@ -39,8 +39,8 @@
 //! #[derive(Serialize, Deserialize)]
 //! # #[derive(PartialEq, Debug)]
 //! struct WithIp {
-//! 	#[serde(with = "serde_str::opt", default)]
-//! 	ip: Option<IpAddr>,
+//!     #[serde(with = "serde_str::opt", default)]
+//!     ip: Option<IpAddr>,
 //! }
 //!
 //! # fn main() -> serde_json::Result<()> {
@@ -67,8 +67,8 @@
 //! #[derive(Serialize, Deserialize)]
 //! # #[derive(PartialEq, Debug)]
 //! struct WithIp {
-//! 	#[serde(with = "serde_str::opt", default, skip_serializing_if = "Option::is_none")]
-//! 	ip: Option<IpAddr>,
+//!     #[serde(with = "serde_str::opt", default, skip_serializing_if = "Option::is_none")]
+//!     ip: Option<IpAddr>,
 //! }
 //!
 //! # fn main() -> serde_json::Result<()> {
@@ -78,39 +78,98 @@
 //! # Ok(())
 //! # }
 //! ```
-use serde::{
-	de,
-	Deserialize,
-	Deserializer,
-	Serialize,
-	Serializer,
-};
-use std::{
-	fmt::Display,
-	str::FromStr,
-};
+
+use std::fmt::{self, Display};
+use std::marker::PhantomData;
+use std::str::FromStr;
+
+use serde::de::{self, Visitor};
+use serde::{Deserializer, Serialize, Serializer};
+
+struct OptVisitor<T>(PhantomData<T>);
+struct StrVisitor<T>(PhantomData<T>);
+
+impl<'de, T: FromStr> Visitor<'de> for StrVisitor<T>
+    where T::Err: Display
+{
+    type Value = Option<T>;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("string")
+    }
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+    fn visit_borrowed_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(&v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+}
+
+impl<'de, T: FromStr> Visitor<'de> for OptVisitor<T>
+    where T::Err: Display
+{
+    type Value = Option<T>;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("optional string")
+    }
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+    fn visit_borrowed_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        T::from_str(&v).map_err(de::Error::custom).map(|s| Some(s))
+    }
+    fn visit_none<E>(self) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        Ok(None)
+    }
+    fn visit_unit<E>(self) -> Result<Self::Value, E>
+        where E: de::Error
+    {
+        Ok(None)
+    }
+    fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+        where D: Deserializer<'de>
+    {
+        deserializer.deserialize_str(StrVisitor::<T>(PhantomData))
+    }
+}
+
 /// Deserialize function, see [mod docs examples](https://docs.rs/serde_str/*/serde_str/opt/index.html) to see how to use it
 pub fn deserialize<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
 where
-	T: FromStr,
-	T::Err: Display,
-	D: Deserializer<'de>,
+    T: FromStr,
+    T::Err: Display,
+    D: Deserializer<'de>,
 {
-	if let Some(s) = Option::deserialize(deserializer)? {
-		T::from_str(s).map_err(de::Error::custom).map(|s| Some(s))
-	} else {
-		Ok(None)
-	}
+    deserializer.deserialize_option(OptVisitor::<T>(PhantomData))
 }
 
 /// Serialize function, see [mod docs examples](https://docs.rs/serde_str/*/serde_str/opt/index.html) to see how to use it
 pub fn serialize<T, S>(
-	value: &Option<T>,
-	serializer: S,
+    value: &Option<T>,
+    serializer: S,
 ) -> Result<S::Ok, S::Error>
 where
-	T: ToString,
-	S: Serializer,
+    T: ToString,
+    S: Serializer,
 {
-	Option::serialize(&value.as_ref().map(|ty| ty.to_string()), serializer)
+    Option::serialize(&value.as_ref().map(|ty| ty.to_string()), serializer)
 }
